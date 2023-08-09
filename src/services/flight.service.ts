671d14d3d48162f9589d196e0214fc89d1dbbc66
Roi C <roicou@gmail.com>
2023-08-09 09:53:08 +0200

    cambios para personalizar códigos de voo

--- src/services/flight.service.ts
+++ src/services/flight.service.ts
@@ -99,8 +99,10 @@ class FlightService {
         // set published to true
         await flightModel.updateMany({ _id: { $in: flights.map((flight: FlightInterface) => flight._id) } }, { published: true });
         flights.forEach((flight: FlightInterface) => {
-            if(flight.companyCode === 'NT' && flight.companyName === 'Binter Canarias') {
-                flight.companyCode = 'IBB';
+            switch(flight.companyCode) {
+                case 'NT':
+                    flight.companyCode = 'IBB';
+                    break;
             }
         });
         return flights;
